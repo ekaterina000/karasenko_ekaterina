a1832bc991a792303e8b6f5d53d307e911ac1300
ekaterina000 <karasenko.03@gmail.com>
2022-03-25 19:13:30 +0300

    создала новый тест DoubleLinkedList_checkGetSize

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -1,14 +1,29 @@
 import org.junit.*;
 public class Task1_Tests extends Assert {
     @Test
-    public void DoubleLinkedList_CheckIsEmpty(){
+    public void DoubleLinkedList_CheckIsEmpty() {
         DoubleLinkedList<Integer> List = new DoubleLinkedList<>();
         assertTrue(List.isEmpty());
     }
+
     @Test
-    public void CrateNewDoubleLinkedList() {
+    public void DoubleLinkedList_CrateNewDoubleLinkedList() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(list.isEmpty());
     }
+
+    @Test
+    public void DoubleLinkedList_PushFrontToEmptyList() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(1);
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void DoubleLinkedList_checkGetSize() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        assertTrue(0 == list.getSize());
+    }
 }
+
 
