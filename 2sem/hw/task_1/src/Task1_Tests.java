acd3ee6e5aae16fcd1a33844d25fc7f1be7f327d
ekaterina000 <karasenko.03@gmail.com>
2022-03-26 20:24:05 +0300

    создала тест DoubleLinkedList_CheckIsEmpty

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -6,24 +6,5 @@ public class Task1_Tests extends Assert {
         assertTrue(List.isEmpty());
     }
 
-    @Test
-    public void DoubleLinkedList_CrateNewDoubleLinkedList() {
-        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
-        assertTrue(list.isEmpty());
-    }
-
-    @Test
-    public void DoubleLinkedList_PushFrontToEmptyList() {
-        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
-        list.pushFront(1);
-        assertFalse(list.isEmpty());
-    }
-
-    @Test
-    public void DoubleLinkedList_checkGetSize() {
-        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
-        assertTrue(0 == list.getSize());
-    }
 }
-
 
