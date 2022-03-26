50ae811939efc95d783595391754754966ccc0af
ekaterina000 <karasenko.03@gmail.com>
2022-03-27 01:56:06 +0300

    создан тест create_CreateList_EmptyFalse

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -7,4 +7,10 @@ public class Task1_Tests extends Assert{
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(list.isEmpty());
     }
+    @Test
+    public void create_CreateList_EmptyFalse() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushBack(1);
+        assertFalse(list.isEmpty());
+    }
 }
