debb7b44939d0aee208f9aa2eaf5e39c3b944745
ekaterina000 <karasenko.03@gmail.com>
2022-03-27 00:03:42 +0300

    hw

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -2,8 +2,18 @@ import org.junit.*;
 
 public class Task1_Tests extends Assert {
     @Test
+<<<<<<< HEAD
     public void DoubleLinkedList_createEmpty() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(list.isEmpty());
+=======
+    public void DoubleLinkedList_CheckIsEmpty() {
+        DoubleLinkedList<Integer> List = new DoubleLinkedList<>();
+        assertTrue(List.isEmpty());
+>>>>>>> parent of d96299e (создала тест DoubleLinkedList_checkIsEmpty)
     }
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of d96299e (создала тест DoubleLinkedList_checkIsEmpty)
