53c9499168859f65f5b80ad9165c0e1a95276322
ekaterina000 <karasenko.03@gmail.com>
2022-03-27 00:23:14 +0300

    hw

--- 2sem/hw/task_1/src/Task1_Tests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.*;
-
-public class Task1_Tests extends Assert {
-    @Test
-    public void createList_EmptyTrue(){
-        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
-        assertTrue(list.isEmpty());
-    }
-}
