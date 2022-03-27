b7547037ef15de47692bfcab2524a47c97eee9c5
ekaterina000 <karasenko.03@gmail.com>
2022-03-27 14:14:42 +0300

    создан тест insert_InsertBounds_CheckException

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -210,6 +210,17 @@ public class Task1_Tests extends Assert {
                 assertEquals("Index out of bounds", some.getMessage());
             }
         }
+    @Test
+    public void insert_InsertBounds_CheckException(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.insert(6,10);
+            fail();
+        }
+        catch (IndexOutOfBoundsException some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
     }
 
 
