35161d297701f8a175f23e3b7f1493d9bc96f0b4
ekaterina000 <karasenko.03@gmail.com>
2022-03-27 13:07:13 +0300

    создан тест insert_insertListAfter_CheckValues

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -70,6 +70,31 @@ public class Task1_Tests extends Assert {
         int head = ((Node<Integer>)list.getHead()).getData();
         assertEquals(1, head);
     }
+    @Test
+    public void insert_insertListAfter_CheckValues(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(1);
+        list.pushFront(5);
+        list.pushFront(6);
 
+        DoubleLinkedList<Integer> list1 = new DoubleLinkedList<>();
+        list1.pushFront(2);
+        list1.pushFront(3);
+        list1.pushFront(4);
+        list.insertListAfter(list.get(0), list1);
+
+        int num1 = ((Node<Integer>)list.get(0)).getData();
+        assertEquals(1, num1);
+        num1 = ((Node<Integer>)list.get(1)).getData();
+        assertEquals(2, num1);
+        num1 = ((Node<Integer>)list.get(2)).getData();
+        assertEquals(3, num1);
+        num1 = ((Node<Integer>)list.get(3)).getData();
+        assertEquals(4, num1);
+        num1 = ((Node<Integer>)list.get(4)).getData();
+        assertEquals(5, num1);
+        num1 = ((Node<Integer>)list.get(5)).getData();
+        assertEquals(6, num1);
+    }
 }
 
