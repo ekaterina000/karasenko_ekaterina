3eb12d37c10904d88a59813b4fc0f17623a64391
ekaterina000 <karasenko.03@gmail.com>
2022-03-31 00:11:16 +0300

    add quickSort_UnsortedArray_ArrayIsCorrect

--- 2sem/hw/task_3/src/Task3_Tests.java
+++ 2sem/hw/task_3/src/Task3_Tests.java
@@ -8,4 +8,11 @@ public class Task3_Tests extends Assert {
             return first - second;
         }
     }
+    @Test
+    public void quickSort_UnsortedArray_ArrayIsCorrect(){
+        Integer[] arr = new Integer[]{9,2};
+        QuickSort<Integer> sorter = new QuickSort();
+        sorter.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{2, 9}, arr);
+    }
 }
