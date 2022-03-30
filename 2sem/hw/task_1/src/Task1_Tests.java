0e6622925ce6ea8f33e72670366cdf20f50a48f8
ekaterina000 <karasenko.03@gmail.com>
2022-03-30 15:50:20 +0300

    add getHead_headFromList_Head

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -13,17 +13,20 @@ public class Task1_Tests extends Assert {
         list.pushBack(1);
         assertFalse(list.isEmpty());
     }
+
     @Test
     public void getSize_sizeList_checkSize() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(0 == list.getSize());
     }
+
     @Test
     public void pushFront_pushFrontList_checkNumber() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         list.pushFront(10);
         assertTrue(1 == list.getSize());
     }
+
     @Test
     public void getNumber_getNumberList_checkNumber() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
@@ -34,5 +37,14 @@ public class Task1_Tests extends Assert {
         } catch (IndexOutOfBoundsException e) {
             assertFalse(false);
         }
+    }
+
+    @Test
+    public void getHead_headFromList_Head() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(5);
+        list.pushFront(2);
+        int y = ((Node<Integer>) list.getHead()).getData();
+        assertEquals(5, y);
     }
 }
