cc6dcac65d291f1f7dd6dd4005cba3399aa995ea
ekaterina000 <karasenko.03@gmail.com>
2022-03-30 22:14:55 +0300

    add test SelectionSort_SortArray_ArrayIsCorrect

--- 2sem/hw/task_2/src/Task2_Tests.java
+++ 2sem/hw/task_2/src/Task2_Tests.java
@@ -13,9 +13,7 @@ public class Task2_Tests extends Assert {
     public void BubbleSort_sortTheSameNumbers_IsCorrect() {
         BubbleSort<Integer> sorter = new BubbleSort<>();
         Integer[] bubbleArray = {1, 1, 1};
-
         sorter.sort(bubbleArray, new TestComparator());
-
         assertArrayEquals(new Integer[]{1, 1, 1}, bubbleArray);
     }
 
@@ -23,18 +21,14 @@ public class Task2_Tests extends Assert {
     public void BubbleSort_rightOrder_NumbersInRightOrder() {
         BubbleSort<Integer> sorter = new BubbleSort<>();
         Integer[] bubbleArray = {2, 4, 6};
-
         sorter.sort(bubbleArray, new TestComparator());
-
         assertArrayEquals(new Integer[]{2, 4, 6}, bubbleArray);
     }
     @Test
     public void BubbleSort_SortData_DataIsCorrect() {
         BubbleSort<Integer> sorter = new BubbleSort<>();
         Integer[] bubbleArray = {1, 5, 3};
-
         sorter.sort(bubbleArray, new TestComparator());
-
         assertArrayEquals(new Integer[]{1, 3, 5}, bubbleArray);
     }
 
@@ -45,9 +39,7 @@ public class Task2_Tests extends Assert {
     public void SelectionSort_InvertedArray_ArrayIsCorrect() {
         SelectionSort<Integer> sorter = new SelectionSort<>();
         Integer[] selectionArray = {4, 2, 1};
-
         sorter.sort(selectionArray, new TestComparator());
-
         assertArrayEquals(new Integer[]{1, 2, 4}, selectionArray);
     }
 
@@ -55,10 +47,16 @@ public class Task2_Tests extends Assert {
     public void SelectionSort_UnsortedArray_ArrayIsCorrect() {
         SelectionSort<Integer> sorter = new SelectionSort<>();
         Integer[] selectionArray = {10, 5};
-
         sorter.sort(selectionArray, new TestComparator());
-
         assertArrayEquals(new Integer[]{5, 10}, selectionArray);
+    }
+
+    @Test
+    public void SelectionSort_SortArray_ArrayIsCorrect() {
+        SelectionSort<Integer> sorter = new SelectionSort<>();
+        Integer[] selectionArray = {75,3,94,7};
+        sorter.sort(selectionArray, new TestComparator());
+        assertArrayEquals(new Integer[]{3, 7, 75, 94}, selectionArray);
     }
 
 }
