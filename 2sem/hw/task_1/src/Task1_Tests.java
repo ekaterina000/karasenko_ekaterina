27e11061ca300c1e66f4766104217978d7282559
ekaterina000 <karasenko.03@gmail.com>
2022-03-30 15:38:03 +0300

    add check_checkList_isEmpty

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -1,2 +1,9 @@
 import org.junit.*;
-public class Task1_Tests extends Assert {}
+public class Task1_Tests extends Assert {
+
+    @Test
+    public void check_checkList_isEmpty() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        assertTrue(list.isEmpty());
+    }
+}
