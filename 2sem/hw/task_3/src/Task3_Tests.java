78bdbfe8c225709850d060b56e2f6e713aebb0e3
ekaterina000 <karasenko.03@gmail.com>
2022-03-31 00:05:52 +0300

    add class TestComparator

---
+++ 2sem/hw/task_3/src/Task3_Tests.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.*;
+
+public class Task3_Tests extends Assert {
+    class TestComparator implements MyComparator<Integer> {
+        @Override
+        public int compare(Integer first, Integer second) {
+            return first - second;
+        }
+    }
+}
