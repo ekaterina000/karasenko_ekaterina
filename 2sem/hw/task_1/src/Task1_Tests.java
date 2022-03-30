74c2c92e87ba0a740fae67d5de443ff63caba9b7
ekaterina000 <karasenko.03@gmail.com>
2022-03-30 19:57:22 +0300

    error corrected

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -6,4 +6,10 @@ public class Task1_Tests extends Assert {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(list.isEmpty());
     }
+    @Test
+    public void check_checkList_isEmptyFalse(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        Node<Integer> elem = (Node<Integer>) list.pushBack(3);
+        assertEquals(false, list.isEmpty());
+    }
 }
