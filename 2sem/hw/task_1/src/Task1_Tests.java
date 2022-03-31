02bf1790a8d4d9428527f94f76eb0a3d87a9be63
ekaterina000 <karasenko.03@gmail.com>
2022-03-31 17:44:57 +0300

    add pushFront_pushFrontToEmptyList_ListIsCorrect

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -18,4 +18,14 @@ public class Task1_Tests extends Assert{
         list.pushFront(10);
         assertEquals(1, list.getSize());
     }
+    @Test
+    public void pushFront_pushFrontToEmptyList_ListIsCorrect(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(10);
+        Node<Integer> head = (Node<Integer>) list.getHead();
+        Node<Integer> tail = (Node<Integer>) list.getTail();
+        assertEquals(10, head.getData().intValue());
+        assertEquals(10, tail.getData().intValue());
+        assertEquals(head, tail);
+    }
 }
