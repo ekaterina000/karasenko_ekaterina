59c16bdded620b93d7fc2988809a0d7728763192
ekaterina000 <karasenko.03@gmail.com>
2022-03-31 17:34:05 +0300

    add getNext_getNextData_ListIsCorrect

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -1,3 +1,15 @@
 import org.junit.*;
 
-public class Task1_Tests extends Assert{}
+public class Task1_Tests extends Assert{
+    @Test
+    public void getNext_getNextData_ListIsCorrect(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        Node<Integer> elem1 = list.pushFront(1);
+        Node<Integer> elem3 = list.pushFront(3);
+        Node<Integer> elem = new Node<Integer>(2, elem3, elem1);
+        Node<Integer> next = (Node<Integer>) elem.getNext();
+        Node<Integer> prev = (Node<Integer>) elem.getPrev();
+        assertEquals(3, next.getData().intValue());
+        assertEquals(1, prev.getData().intValue());
+    }
+}
